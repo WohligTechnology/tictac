d2b8b4b329e701deafbb8848a88a25006cda74ec
sohan1 <shn619@gmail.com>
2015-10-02 18:19:06 +0530

    Ting first git

---
+++ js/controllers.js
@@ -0,0 +1,19 @@
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngSanitize', 'angular-flexslider'])
+
+.controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+  //Used to name the .html file
+  $scope.template = TemplateService.changecontent("home");
+  $scope.menutitle = NavigationService.makeactive("Home");
+  TemplateService.title = $scope.menutitle;
+  $scope.navigation = NavigationService.getnav();
+
+})
+
+.controller('FeatureCtrl', function($scope, TemplateService) {
+  $scope.template = TemplateService;
+})
+.controller('headerctrl', function($scope, TemplateService) {
+  $scope.template = TemplateService;
+})
+
+;
