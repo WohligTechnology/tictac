d58cb96d4d5ba0b136aa5c0194f7db628ce2576c
sohan1 <shn619@gmail.com>
2015-10-13 11:22:24 +0530

    ting

--- js/controllers.js
+++ js/controllers.js
@@ -6,7 +6,6 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   $scope.menutitle = NavigationService.makeactive("Home");
   TemplateService.title = $scope.menutitle;
   $scope.navigation = NavigationService.getnav();
-
 })
 
 .controller('FeatureCtrl', function($scope, TemplateService) {
