5beddd359a98a391dfcfd9ff3f87bf918962d2fb
sohan1 <shn619@gmail.com>
2015-11-10 20:00:40 +0530

    done

--- js/app.js
+++ js/app.js
@@ -97,12 +97,12 @@ firstapp.directive('ngScrollDuration', function ($compile, $parse) {
 					.setPin(pin, {
 						pushFollowers: false
 					})
-					.addIndicators() // add indicators (requires plugin)
+//					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
 			} else {
 				var scene = new ScrollMagic.Scene(animation)
 					.setTween(tween)
-					.addIndicators() // add indicators (requires plugin)
+//					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
 			}
 		}
