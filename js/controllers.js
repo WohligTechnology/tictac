42c737f411f82de082a3f6ee11b89c2434bb7b09
sohan1 <shn619@gmail.com>
2015-11-17 14:57:42 +0530

    hello

--- js/controllers.js
+++ js/controllers.js
@@ -213,7 +213,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
 	$scope.gotoBottom = function () {
 		// set the location.hash to the id of
 		// the element you wish to scroll to.
-		$location.hash('bottom');
+		$location.hash('hello');
 
 		// call $anchorScroll()
 		$anchorScroll();
