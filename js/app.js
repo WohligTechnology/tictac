331ac33806f85cc82380244444141e175b33ff30
Harsh Thakkar <harshthakkar46@gmail.com>
2015-11-27 18:57:15 +0530

    mobile changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // JavaScript Document
+var ZoomValue = 1;
+var HeightChange  = 1;
 var controller = new ScrollMagic.Controller();
+var Section8Height = 800;
 var firstapp = angular.module('firstapp', [
     'ui.router',
     'phonecatControllers',
@@ -20,16 +23,9 @@ firstapp.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 		controller: 'HomeCtrl'
 	})
 
-	.state('feature', {
-		url: "/feature",
-		templateUrl: "views/template.html",
-		controller: 'FeatureCtrl'
-	})
-
 	$urlRouterProvider.otherwise("/home");
 
 });
-
 
 firstapp.directive('typedJs', function ($document) {
 	return {
@@ -41,6 +37,23 @@ firstapp.directive('typedJs', function ($document) {
 			dem = $element;
 			$element.typed({
 				strings: ["ios!", "Android!", "windows?", "blackberry :P"],
+				startDelay: 20,
+				typeSpeed: 100,
+				loop: true
+			});
+		}
+	}
+});
+
+firstapp.directive('loadingText', function ($document) {
+	return {
+		restrict: 'EA',
+		replace: false,
+		link: function ($scope, element, attrs) {
+			var $element = $(element);
+			dem = $element;
+			$element.typed({
+				strings: ["loading...", "we're almost there...", "don't dig your nose...", "don't bounce away...","almost there..."],
 				startDelay: 20,
 				typeSpeed: 100,
 				loop: true
@@ -70,41 +83,49 @@ firstapp.directive('img', function ($compile, $parse) {
 });
 
 
-firstapp.directive('ngScrollDuration', function ($compile, $parse) {
+
+firstapp.directive('toggleClass', function ($compile, $parse) {
 	return {
 		restrict: 'EA',
 		replace: false,
 		link: function ($scope, element, attrs) {
-			var $element = $(element);
-			var animation = {};
-			var animationSceneFrom = eval("(" + attrs.ngScrollAnimationFrom + ")");
-			var animationSceneTo = eval("(" + attrs.ngScrollAnimationTo + ")");
-			var id = attrs.id;
-			var pin = attrs.pin;
-			var toggleClass = attrs.toggleClass;
-			if (attrs.ngScrollTrigger) {
-				animation.triggerElement = attrs.ngScrollTrigger;
-			}
-			animation.duration = attrs.ngScrollDuration;
-			animation.offset = attrs.ngScrollOffset;
-			// build tween
-			var tween = TweenMax.fromTo($element, 1, animationSceneFrom, animationSceneTo);
 
-			// build scene
-			if (pin) {
-				var scene = new ScrollMagic.Scene(animation)
-					.setTween(tween)
-					.setPin(pin, {
-						pushFollowers: false
-					})
-					.addIndicators() // add indicators (requires plugin)
+      console.log(attrs);
+
+			var $element = $(element);
+			var triggerElement = attrs.triggerElement;
+      console.log(triggerElement);
+      new ScrollMagic.Scene({triggerElement: triggerElement})
+					.setClassToggle(attrs.toggleElement, attrs.toggleClass) // add class toggle
+      //    .addIndicators()
 					.addTo(controller);
-			} else {
-				var scene = new ScrollMagic.Scene(animation)
-					.setTween(tween)
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
-			}
+
+		}
+	};
+});
+
+var taxiAni = true;
+firstapp.directive('taxiAnimation', function ($compile, $parse) {
+	return {
+		restrict: 'EA',
+		replace: false,
+		link: function ($scope, element, attrs) {
+      new ScrollMagic.Scene({triggerElement: ".taxiAnimation", duration: 200})
+                .addTo(controller)
+                //.addIndicators() // add indicators (requires plugin)
+                .on("enter", function (e) {
+                  console.log("Taxi Enter");
+                  setTimeout(function() {
+                    taxiAni = true;
+                  }, 6000);
+                  if(taxiAni == true)
+                  {
+                    $(".taxiAnimation").attr("src","img/gif/taxianimation.gif");
+                  }
+                  taxiAni = false;
+
+
+                })
 		}
 	};
 });
@@ -116,6 +137,19 @@ firstapp.filter('serverimage', function () {
 			return adminimage + image;
 		} else {
 			return undefined;
+		}
+
+	};
+});
+
+
+firstapp.filter('extlink', function () {
+	return function (link) {
+    var link2 = link.substring(0, 4);
+		if (link2 == "http") {
+			return link
+		} else {
+			return "http://"+link;
 		}
 
 	};
@@ -137,4 +171,31 @@ firstapp.directive('youtube', function ($sce) {
 			});
 		}
 	};
+});
+
+
+
+
+
+
+function changeZoom() {
+    var newwidth = $(window).width();
+    ZoomValue= newwidth/1349;
+    //ZoomValue  = 1;
+    $("body").css("zoom",ZoomValue);
+    HeightChange = $(window).height()/667;
+
+
+    var mainHeight = $(window).height();
+    section8Height  = $(".section8").height() * ZoomValue;
+    var lastHeightDiff = mainHeight - section8Height;
+    Section8Height = mainHeight/ZoomValue;
+}
+changeZoom();
+$(document).ready(function() {
+
+  changeZoom();
+  $(window).resize(function() {
+    changeZoom();
+  });
 });
